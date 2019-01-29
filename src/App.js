d70a7b17e461a66da1e176e402535e65ec136240
Rashmi Kamath <rashmikkamath@gmail.com>
2019-01-28 19:25:38 -0500

    Container for blocks that performs the fetch of data from api.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import BlocksContainer from './components/BlocksContainer'
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div>
         <div>A Generic Hello World text</div>
+        <BlocksContainer/>
       </div>
     );
   }
