41cb4a8dc5d702a820a0bdc24ca32e84b248b1df
Rashmi Kamath <rashmikkamath@gmail.com>
2019-01-30 14:52:17 -0500

    Using React Router to enable navigation through components. Simpler to enable Back button functionality.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import BlocksContainer from './components/BlocksContainer'
+// Import routing components
+import { Router, Route } from 'react-router-dom';
+import BlocksContainer from './components/BlocksContainer';
+import DetailsContainer from './components/DetailsContainer';
+import createBrowserHistory from 'history/createBrowserHistory';
 import './App.css';
+const history = createBrowserHistory();
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div>A Generic Hello World text</div>
-        <BlocksContainer/>
+        <Router history={history}>
+          <div>
+            <Route path='/' exact component={BlocksContainer} />
+            <Route path='/details' component={DetailsContainer} />
+          </div>
+        </Router>
       </div>
+      
     );
   }
 }
